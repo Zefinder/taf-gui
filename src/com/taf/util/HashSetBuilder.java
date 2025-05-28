aa1d947ed5c03c081b83a588ea33617021977a89
Zefinder <jakubiak@insa-toulouse.fr>
2025-05-28 11:02:44 +0200

    Create an ordered hash set instead of a simple hash set

--- src/com/taf/util/HashSetBuilder.java
+++ src/com/taf/util/HashSetBuilder.java
@@ -1,13 +1,14 @@
 package com.taf.util;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class HashSetBuilder<T> {
 
 	private HashSet<T> elementSet;
 
 	public HashSetBuilder() {
-		elementSet = new HashSet<T>();
+		elementSet = new LinkedHashSet<T>();
 	}
 
 	public HashSetBuilder<T> add(T element) {
