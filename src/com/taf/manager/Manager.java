8042aad4e6703b84c1c8e18184227c0a5f578d99
Zefinder <jakubiak@insa-toulouse.fr>
2025-09-01 11:23:19 +0200

    Modified Manager to search for managers in a jar file

--- src/com/taf/manager/Manager.java
+++ src/com/taf/manager/Manager.java
@@ -36,10 +36,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Stream;
 
 import com.taf.annotation.ManagerImpl;
 
@@ -72,31 +78,51 @@ public interface Manager {
 	 * Initializes all classes with annotation ManagerImpl and having no public
 	 * constructors, a static method getInstance that returns itself with no
 	 * parameters.
+	 * 
+	 * @throws IOException
 	 */
-	public static void initManagers() {
+	public static void initManagers() throws IOException {
 		var annotationClass = ManagerImpl.class;
 		String packageName = Manager.class.getPackage().getName();
-		InputStream stream = ClassLoader.getSystemResourceAsStream(packageName.replaceAll("[.]", "/"));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-		reader.lines()
-				.filter(line -> line.endsWith(".class"))
-				.<Optional<Class<?>>>map(line -> { // Gets all classes within the com.taf.manager package
-					try {
-						return Optional.of(Class.forName(packageName + "." + line.substring(0, line.lastIndexOf('.'))));
-					} catch (ClassNotFoundException e) {
-						return Optional.empty();
-					}
-				})
-				.filter(clazzOptional -> clazzOptional.isPresent() // Filters classes that has the annotation ManagerImpl
-						&& clazzOptional.get().isAnnotationPresent(annotationClass)) 
-				.map(Optional::get)
+		Stream<String> classFileStream;
+
+		URL codeSourceURL = Manager.class.getProtectionDomain().getCodeSource().getLocation();
+		if (codeSourceURL.toString().endsWith(".jar")) {
+			// Resource in a JAR
+			JarFile jarFile = new JarFile(codeSourceURL.getFile());
+			Enumeration<JarEntry> entries = jarFile.entries();
+			classFileStream = Collections.list(entries).stream().map(entry -> entry.getName())
+					.filter(name -> name.startsWith(packageName.replaceAll("[.]", "/")))
+					.map(name -> name.substring(name.lastIndexOf('/') + 1));
+			jarFile.close();
+		} else {
+			// Try to directly access resource
+			InputStream stream = ClassLoader.getSystemResourceAsStream(packageName.replaceAll("[.]", "/"));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			classFileStream = reader.lines();
+		}
+		
+		classFileStream.filter(line -> line.endsWith(".class")).<Optional<Class<?>>>map(line -> { // Gets all classes
+																									// within the
+																									// com.taf.manager
+																									// package
+			try {
+				return Optional.of(Class.forName(packageName + "." + line.substring(0, line.lastIndexOf('.'))));
+			} catch (ClassNotFoundException e) {
+				return Optional.empty();
+			}
+		}).filter(clazzOptional -> clazzOptional.isPresent() // Filters classes that has the annotation ManagerImpl
+				&& clazzOptional.get().isAnnotationPresent(annotationClass)).map(Optional::get)
 				.filter(clazz -> clazz.getConstructors().length == 0 // Filters classes with no constructors
 						&& Arrays.asList(clazz.getMethods()).stream()
-															.anyMatch(method -> method.getName().equals("getInstance") // Method must be named getInstance
-																	&& method.getParameterCount() == 0 // with no parameters
-																	&& clazz.isAssignableFrom(method.getReturnType()) // that returns itself
-																	&& Modifier.isStatic(method.getModifiers()))) // and is static
-				.sorted((managerClass1, managerClass2) -> -managerClass1.getAnnotation(annotationClass).priority() // Sort by priority
+								.anyMatch(method -> method.getName().equals("getInstance") // Method must be named
+																							// getInstance
+										&& method.getParameterCount() == 0 // with no parameters
+										&& clazz.isAssignableFrom(method.getReturnType()) // that returns itself
+										&& Modifier.isStatic(method.getModifiers()))) // and is static
+				.sorted((managerClass1, managerClass2) -> -managerClass1.getAnnotation(annotationClass).priority() // Sort
+																													// by
+																													// priority
 						.compareTo(managerClass2.getAnnotation(annotationClass).priority()))
 				.<Optional<? extends Manager>>map(managerClass -> { // Invoke getInstance
 					try {
@@ -106,17 +132,12 @@ public interface Manager {
 						// Should never happen
 						return Optional.empty();
 					}
-				})
-				.filter(Optional::isPresent).map(Optional::get)
-				.forEach(manager -> { // Init manager and add to the manager set
+				}).filter(Optional::isPresent).map(Optional::get).forEach(manager -> { // Init manager and add to the
+																						// manager set
 					manager.init();
+					System.out.println(manager);
 					MANAGERS.add(manager);
 				});
-		try {
-			reader.close();
-		} catch (IOException e) {
-			// Ignored
-		}
 	}
 
 	/**
@@ -130,5 +151,5 @@ public interface Manager {
 		// Reset the manager set
 		MANAGERS.clear();
 	}
-	
+
 }
